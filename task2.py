c6874f5c0146344b95e00f04a7c510003725764a
Rashid20071601 <153351715+Rashid20071601@users.noreply.github.com>
2024-09-29 10:02:03 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,11 +1,12 @@
-def get_password(n):
-    if n < 3 or n > 20:
+n = int(input('Введите целое число от 3 до 20: '))
+def get_password(password):
+    if password < 3 or password > 20:
         return "Число должно быть от 3 до 20."
     result = ""
-    for i in range(1, n + 1):
-        for j in range(i + 1, n + 1):
-            if n % (i + j) == 0:
+    for i in range(1, password + 1):
+        for j in range(i + 1, password + 1):
+            if password % (i + j) == 0:
                 result += f"{i}{j} "
     return result
 
-print(get_password(3))
+print('Пароль:', get_password(n))
